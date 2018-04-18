5af928bac337371f63869874ba2868740fa71df0
HanamichiXave <johnxavierbuenaventura@gmail.com>
2018-04-18 17:50:50 +0800

    resigned

---
+++ SchulerPrototype/Scripts/js/Hr/hrResigned.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function(){
+   var oTable = $('#found,#claimed').DataTable({
+     "orderCellsTop": false,
+     "searchHighlight" : true,
+   });
+   $('#search').keyup(function(){
+         oTable.search($(this).val()).draw() ;
+   });
+   $(".dataTables_filter").hide();
+   oTable.on('order.dt search.dt', function(){
+     oTable.column(0, {search:'applied', order:'applied'}).nodes().each(
+      function (cell, i) {
+       cell.innerHTML = i+1;
+       } );
+   }).draw();
+ });
